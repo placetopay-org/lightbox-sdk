1939d887668bd7aec3d6f3151c0a9988c67dc57b
JuanZea <vikzea@gmail.com>
2022-09-27 13:26:16 -0500

    feat: compiler options

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export const LigthboxApp = {
+    mount: () => {
+        console.log('LigthboxApp mounted');
+    }
+};
+export const LigthboxClient = {
+    init: () => {
+        console.log('LigthboxClient initialized');
+    }
+};
